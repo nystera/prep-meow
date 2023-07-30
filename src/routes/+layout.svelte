7d3ece26d91354dd18ffab156e5baabce770c638
Nathan Yeo <yeo.nathan@outlook.com>
2023-07-31 02:32:58 +0800

    fixed: better sidebar animation

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,40 +2,29 @@
   import '../app.css';
   import HamburgerOpenIcon from 'virtual:icons/mdi/hamburger-open';
   import HamburgerIcon from 'virtual:icons/mdi/hamburger-menu';
-  import { cubicOut } from 'svelte/easing';
-  import { tweened } from 'svelte/motion';
+  import { fade } from 'svelte/transition';
 
   let innerWidth = 0;
-  const SIDE_BAR_WIDTH = 30;
-
-  const sideBarWidth = tweened(SIDE_BAR_WIDTH, {
-    duration: 500,
-    easing: cubicOut
-  });
-  $: menuOpened = $sideBarWidth > 0;
-
-  const openMenu = () => {
-    sideBarWidth.set(SIDE_BAR_WIDTH);
-  };
-
-  const closeMenu = () => {
-    sideBarWidth.set(0);
-  };
+  let headerHeight = 0;
+  let isSidebarOpened = innerWidth >= 769;
 </script>
 
 <svelte:window bind:innerWidth />
 
 <div>
-  <header class="flex sticky top-0 bg-surface pt-5">
+  <header
+    class="flex sticky top-0 bg-surface pt-5"
+    bind:clientHeight={headerHeight}
+  >
     <div
       class="flex w-full bg-surface-20 border-2 border-primary rounded-xl mx-5 p-4 justify-between items-center
 			tablet:border-4"
     >
       <button
         class="text-xl laptop:text-3xl"
-        on:click={() => (menuOpened ? closeMenu() : openMenu())}
+        on:click={() => (isSidebarOpened = !isSidebarOpened)}
       >
-        {#if menuOpened}
+        {#if isSidebarOpened}
           <HamburgerOpenIcon />
         {:else}
           <HamburgerIcon />
@@ -49,14 +38,14 @@
 						laptop:text-5xl"
           href="/"
         >
-          PrepMeow
+          prep meow
         </a>
         <span
           class="text-white font-semibold text-2xl text-center
 						tablet:text-3xl
 						laptop:text-5xl"
         >
-          PrepMeow
+          prep meow
         </span>
       </div>
       <a
@@ -73,17 +62,42 @@
   </header>
   <div class="flex gap-5">
     <nav
-      class="hidden laptop:flex laptop:sticky flex-shrink-2 top-32 h-[70vh]
-				border-2 border-primary rounded-r-lg shadow-md shadow-primary"
-      style="width: {$sideBarWidth}rem;"
-    />
-    <div class="flex shrink-3">
+      class={`hidden laptop:flex laptop:flex-col flex-shrink-0 laptop:sticky h-[70vh] w-0 opacity-0 scroll-mt-5
+				border-2 border-primary rounded-r-lg shadow-md shadow-primary mt-5
+				transition-all ease-out duration-200`}
+      class:expanded={isSidebarOpened}
+      style={`top: calc(${headerHeight}px + 1.25rem);`}
+    >
+      {#if isSidebarOpened}<a
+          href="/flash/frontend-developer"
+          in:fade={{ duration: 100, delay: 0 }}
+          out:fade={{ duration: 100, delay: 0 }}
+        >
+          Frontend Developer
+        </a>
+      {/if}
+      {#if isSidebarOpened}
+        <a
+          href="/flash/backend-developer"
+          in:fade={{ duration: 100, delay: 100 }}
+          out:fade={{ duration: 100, delay: 0 }}
+        >
+          Backend Developer
+        </a>
+      {/if}
+    </nav>
+    <div class="">
       <slot />
     </div>
   </div>
 </div>
 
 <style lang="postcss">
+  .expanded {
+    transition: ease-out 200ms;
+    width: 200px;
+    opacity: 1;
+  }
   :global(body) {
     background-color: theme(colors.surface);
     font-family: theme(fontFamily.primary);
